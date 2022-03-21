b6fd4b33a964d2e43de84b0dcb97298b11a483d6
ethangingrich <egingric@uwo.ca>
2022-03-21 17:32:48 -0400

    Added functionallity of melee attacks

---
+++ Assets/__Scripts/MeleeAttack.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    private float timeBtwnAttack;
+    public float startTimeBtwnAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public float damage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwnAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.X))
+            {
+                Collider2D[] enemiesToDmg = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for(int i = 0; i< enemiesToDmg.Length; i++)
+                {
+                    enemiesToDmg[i].GetComponent<Health>().UpdateHealth(-damage);
+                }
+
+            }
+            timeBtwnAttack = startTimeBtwnAttack;
+        }
+        else
+        {
+            timeBtwnAttack -= Time.deltaTime;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
