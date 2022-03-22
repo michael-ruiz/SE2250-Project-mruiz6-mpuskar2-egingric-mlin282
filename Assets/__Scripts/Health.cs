c90aad8abbfe6e564dc6ec717228926e4039aff7
Michael Ruiz <mruiz6@uwo.ca>
2022-03-22 19:04:08 -0400

    changed health file

--- Assets/__Scripts/Health.cs
+++ Assets/__Scripts/Health.cs
@@ -6,15 +6,31 @@ using UnityEngine.UI;
 public class Health : MonoBehaviour
 {
     public float maxHealth = 100;
+    public bool isAlive;
     private float _health;
     private Slider _healthbar;
 
     // Set player starting health
     void Awake()
     {
+        isAlive = true;
         _health = maxHealth;
         _healthbar = GetComponentInChildren<Slider>();
         _healthbar.value = _health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAlive)
+        {
+            if (gameObject.tag == "Enemy")
+            {
+                //update score text
+            }
+
+            Destroy(gameObject);
+        }
     }
 
     // Increase or decrease health by a given value
@@ -30,15 +46,9 @@ public class Health : MonoBehaviour
         else if (_health <= 0)
         {
             _health = 0;
-            // Player dead, restart?
+            isAlive = false;
         }
 
         _healthbar.value = _health;
-    }
-
-    public float health
-    {
-        get { return _health; }
-        set { _health = value; }
     }
 }
