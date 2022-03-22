4af4f8d4963381b6785cb9ad9a4f9e1783777967
Michael Ruiz <mruiz6@uwo.ca>
2022-03-22 16:50:16 -0400

    added health bar to player

--- Assets/__Scripts/Health.cs
+++ Assets/__Scripts/Health.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public float maxHealth = 100;
     private float _health;
+    private Slider _healthbar;
 
     // Set player starting health
     void Awake()
     {
         _health = maxHealth;
+        _healthbar = GetComponentInChildren<Slider>();
+        _healthbar.value = _health;
     }
 
     // Increase or decrease health by a given value
@@ -28,6 +32,8 @@ public class Health : MonoBehaviour
             _health = 0;
             // Player dead, restart?
         }
+
+        _healthbar.value = _health;
     }
 
     public float health
