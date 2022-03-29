402d6eb64103ff3ea446f0fe6838298edaae6fd2
Michael Ruiz <mruiz6@uwo.ca>
2022-03-28 22:25:22 -0400

    added logic for final door to open

--- Assets/__Scripts/_General/OpenExitDoor.cs
+++ Assets/__Scripts/_General/OpenExitDoor.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class OpenExitDoor : MonoBehaviour
 {
+    private bool _allEnemiesGone = false;
+    private Collider2D _exitDoor;
+    private GameObject[] _enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _exitDoor = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if (_enemies.Length == 0 || _enemies == null)
+        {
+            _allEnemiesGone = true;
+            _exitDoor.offset = new Vector2(_exitDoor.offset.x, _exitDoor.offset.y + 1.5f);
+        }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Add logic for when enemies are dead
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && _allEnemiesGone)
         {
             MainMenu.currentLvl = 2;
             Destroy(collision.gameObject);
