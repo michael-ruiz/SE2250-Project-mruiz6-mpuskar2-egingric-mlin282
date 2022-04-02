3bb40aa3bfc244ee6a752480eb7413fee9822f40
Marko Puskar <mpuskar2@uwo.ca>
2022-04-01 21:11:27 -0400

    Added level up text when the player levels up

--- Assets/__Scripts/_PlayerScripts/PlayerLevelUp.cs
+++ Assets/__Scripts/_PlayerScripts/PlayerLevelUp.cs
@@ -2,15 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PlayerLevelUp : MonoBehaviour
 {
     private Slider _scorebar;
+    private TextMeshProUGUI _levelUpText;
 
     // Start is called before the first frame update
     void Start()
     {
         _scorebar = GameObject.FindGameObjectWithTag("Score").GetComponent<Slider>();
+        _levelUpText = GameObject.FindGameObjectWithTag("LevelUpText").GetComponent<TextMeshProUGUI>();
+        _levelUpText.enabled = false;
     }
 
     // Update is called once per frame
@@ -26,6 +30,10 @@ public class PlayerLevelUp : MonoBehaviour
             {
                 // Update health
                 GetComponent<Health>().IncreaseMaxHealth();
+
+                _levelUpText.text = "Level Up! Max health increased";
+                _levelUpText.enabled = true;
+                StartCoroutine(HideLevelUpTextDelay(5));
             }
             else if (GetComponent<Char2Movement>() != null) // Character 2
             {
@@ -33,13 +41,29 @@ public class PlayerLevelUp : MonoBehaviour
                 GetComponent<MeleeAttack>().damage *= 1.1f;
                 GetComponent<MeleeAttack>().baseDamage *= 1.1f;
                 GetComponent<PlayerRangedAttack>().rangeProjectile.GetComponent<Projectile>().damage *= 1.1f;
+
+                _levelUpText.text = "Level Up! Max damage increased";
+                _levelUpText.enabled = true;
+                StartCoroutine(HideLevelUpTextDelay(5));
             }
             else if (GetComponent<PlayerMovement>() != null) // Character 3
             {
                 // Update speed
                 GetComponent<PlayerMovement>().speed *= 1.05f;
                 GetComponent<PlayerMovement>().defaultSpeed *= 1.05f;
+
+                _levelUpText.text = "Level Up! Max speed increased";
+                _levelUpText.enabled = true;
+                StartCoroutine(HideLevelUpTextDelay(5));
             }
         }
+    }
+
+    IEnumerator HideLevelUpTextDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        _levelUpText.enabled = false;
     }
 }
