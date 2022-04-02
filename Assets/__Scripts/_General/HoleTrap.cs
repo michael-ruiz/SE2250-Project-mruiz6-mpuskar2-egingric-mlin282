5e88faabcdc5608cc9770107775a0e0a568d1526
Marko Puskar <mpuskar2@uwo.ca>
2022-04-01 23:24:00 -0400

    Added hole trap

---
+++ Assets/__Scripts/_General/HoleTrap.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleTrap : MonoBehaviour
+{
+    private float _damage = 300;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.GetComponent<Health>().UpdateHealth(-_damage);
+            //Destroy(collision.gameObject);
+        }
+    }
+}
